cbe0ea2a1981a77908c0d56b5e6dc17c91edff12
sakilk130 <sakilk130@gmail.com>
2020-10-06 22:06:38 +0600

    add 01,02_useState

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import './App.css';
-import HookCounter from './components/HookCounter';
-
+import HookCounter from './components/01_useState/HookCounter';
+import HookCounterTwo from './components/02_useState/HookCounterTwo';
 function App() {
   return (
     <div className="App">
-      <HookCounter />
+      {/* <HookCounter /> */}
+
+      <HookCounterTwo />
     </div>
   );
 }
---
+++ src/components/02_useState/HookCounterTwo.jsx
@@ -0,0 +1,24 @@
+import React, { useState } from 'react';
+
+function HookCounterTwo() {
+  const start = 0;
+  const [count, setCount] = useState(start);
+
+  const incrementFive = () => {
+    for (let i = 0; i < 5; i++) {
+      setCount((prevCount) => prevCount + 1);
+    }
+  };
+  return (
+    <div>
+      <h3>Count-({count})</h3>
+      <button onClick={() => setCount(start)}>Reset</button>
+      <button onClick={() => setCount(count + 1)}>Increment+1</button>
+      <button onClick={() => setCount(count - 1)}>Decrement-1</button>
+      {/* <button onClick={() => setCount(count + 5)}>Increment+5</button> */}
+      <button onClick={incrementFive}>Increment+5</button>
+    </div>
+  );
+}
+
+export default HookCounterTwo;

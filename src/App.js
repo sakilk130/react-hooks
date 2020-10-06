ed84d0f986de5c5aa6ca199eefddbcebd45aeb34
sakilk130 <sakilk130@gmail.com>
2020-10-07 00:18:01 +0600

    add 04_useState

--- src/App.js
+++ src/App.js
@@ -3,13 +3,17 @@ import './App.css';
 import HookCounter from './components/01_useState/HookCounter';
 import HookCounterTwo from './components/02_useState/HookCounterTwo';
 import HookCounterThree from './components/03_useState/HookCounterThree';
+import HookCounterFour from './components/04_useState/HookCounterFour';
 function App() {
   return (
     <div className="App">
       {/* <HookCounter /> */}
 
       {/* <HookCounterTwo /> */}
-      <HookCounterThree />
+
+      {/* <HookCounterThree /> */}
+
+      <HookCounterFour />
     </div>
   );
 }
---
+++ src/components/04_useState/HookCounterFour.jsx
@@ -0,0 +1,26 @@
+import React, { useState } from 'react';
+
+function HookCounterFour() {
+  const [items, setItem] = useState([]);
+  const addItem = () => {
+    setItem([
+      ...items,
+      {
+        id: items.length,
+        value: Math.floor(Math.random() * 10 + 1),
+      },
+    ]);
+  };
+  return (
+    <div>
+      <h3>Add random numberin array</h3>
+      <button onClick={addItem}>Add Item</button>
+
+      {items.map((item) => (
+        <h4 key={item.id}>{item.value}</h4>
+      ))}
+    </div>
+  );
+}
+
+export default HookCounterFour;

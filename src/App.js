c03efb2d69f9ac40983e6618c061e5f1e7753271
sakilk130 <sakilk130@gmail.com>
2020-10-07 01:15:00 +0600

    06_useEffect

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import HookCounterTwo from './components/02_useState/HookCounterTwo';
 import HookCounterThree from './components/03_useState/HookCounterThree';
 import HookCounterFour from './components/04_useState/HookCounterFour';
 import UseEffectOne from './components/05_useEffect/UseEffectOne';
-
+import UseEffectTwo from './components/06_useEffect/UseEffectTwo';
 function App() {
   return (
     <div className="App">
@@ -17,7 +17,9 @@ function App() {
 
       {/* <HookCounterFour /> */}
 
-      <UseEffectOne />
+      {/* <UseEffectOne /> */}
+
+      <UseEffectTwo />
     </div>
   );
 }
---
+++ src/components/06_useEffect/UseEffectTwo.jsx
@@ -0,0 +1,23 @@
+import React, { useState, useEffect } from 'react';
+
+function UseEffectTwo() {
+  const [x, setX] = useState(0);
+  const [y, setY] = useState(0);
+
+  const logPosition = (e) => {
+    setX(e.clientX);
+    setY(e.clientX);
+  };
+  useEffect(() => {
+    window.addEventListener('mousemove', logPosition);
+  });
+  return (
+    <div>
+      <h3>
+        X-({x})---Y-({y})
+      </h3>
+    </div>
+  );
+}
+
+export default UseEffectTwo;

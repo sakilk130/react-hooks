81b4904b25520943404c6a4cf7cb132c3b227269
sakilk130 <sakilk130@gmail.com>
2020-10-09 16:29:55 +0600

    complete

--- src/App.js
+++ src/App.js
@@ -21,16 +21,16 @@ import ReducerTwo from './components/5_useReducer/12_useReducer/ReducerTwo';
 import ReducerThree from './components/5_useReducer/13_useReducer/ReducerThree';
 import DataFetchingReducer from './components/5_useReducer/14_useReducer/DataFetchingReducer';
 
-import MemoOne from './components/15_useMemo/MemoOne';
+import MemoOne from './components/6_useMemo/15_useMemo/MemoOne';
 
-import RefOne from './components/16_useRef/RefOne';
-import RefTwo from './components/17_useRef/RefTwo';
+import RefOne from './components/7_useRef/16_useRef/RefOne';
+import RefTwo from './components/7_useRef/17_useRef/RefTwo';
 
-import DocTitleOne from './components/18_customHooks/DocTitleOne';
-import DocTitleTwo from './components/18_customHooks/DocTitleTwo';
-import CustomHookOne from './components/19_customHooks/CustomHookOne';
-import CustomHookTwo from './components/19_customHooks/CustomHookTwo';
-import UserForm from './components/20_userInput/UserForm';
+import DocTitleOne from './components/8_customHooks/18_customHooks/DocTitleOne';
+import DocTitleTwo from './components/8_customHooks/18_customHooks/DocTitleTwo';
+import CustomHookOne from './components/8_customHooks/19_customHooks/CustomHookOne';
+import CustomHookTwo from './components/8_customHooks/19_customHooks/CustomHookTwo';
+import UserForm from './components/8_customHooks/20_userInput/UserForm';
 
 export const firstName = React.createContext();
 export const lastName = React.createContext();
@@ -64,7 +64,7 @@ function App() {
         </lastName.Provider>
       </firstName.Provider> */}
 
-      <ReducerOne />
+      {/* <ReducerOne /> */}
 
       {/* <ReducerTwo /> */}
 
@@ -84,7 +84,7 @@ function App() {
       {/* <CustomHookOne />
       <CustomHookTwo /> */}
 
-      {/* <UserForm /> */}
+      <UserForm />
     </div>
   );
 }
